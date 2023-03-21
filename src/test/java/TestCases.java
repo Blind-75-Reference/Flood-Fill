26f7e3d1c4e9b7072ff28dbc137195b6e442c54d
LiquidPlummer <kyle.plummer@revature.com>
2023-03-21 13:17:58 -0400

    main with test cases

---
+++ src/test/java/TestCases.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCases {
+    Solution solution = new Solution();
+
+    @Test
+    public void testCase1() {
+        int[][] img = new int[][]{{1,1,1},{1,1,0},{1,0,1}};
+        int[][] expected = new int[][]{{2,2,2},{2,2,0},{2,0,1}};
+
+        Assertions.assertEquals(expected, solution.floodFill(img, 1, 1, 2));
+    }
+}
